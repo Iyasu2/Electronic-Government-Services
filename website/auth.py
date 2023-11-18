0b50daa456413b616969dacc28aeb4c9d6aa4041
iyasu2 <iyasuforreal@gmail.com>
2023-11-18 20:15:38 +0300

    flask started and basic landing page2

--- website/auth.py
+++ website/auth.py
@@ -1,14 +1,14 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/signup')
 def signup():
-    return "<p>Sign up</p>"
+    return render_template("signup.html")
 
 @auth.route('/login')
 def login():
-    return "<p>Login</p>"
+    return render_template("login.html", text=True)
 
 @auth.route('/logout')
 def logout():
