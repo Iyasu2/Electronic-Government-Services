898d00de6786b361174b5277fa78524a812b926c
iyasu2 <iyasuforreal@gmail.com>
2023-11-19 17:40:13 +0300

    s

--- website/__init__.py
+++ website/__init__.py
@@ -2,6 +2,11 @@
 this module sets up our flask application
 '''
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+
+db =  SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_app():
     '''
@@ -9,6 +14,8 @@ def create_app():
     '''
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'AB-IYuh' #a better secret key might be needed for real applications
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
@@ -16,4 +23,9 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
     
+    from .models import User, Note
+
+    with app.app_context():
+        db.create_all()
+
     return app
--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,8 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+from passlib.hash import sha256_crypt
 
 auth = Blueprint('auth', __name__)
 
@@ -6,13 +10,18 @@ auth = Blueprint('auth', __name__)
 def signup():
     if request.method == 'POST':
         email = request.form.get('email')
-        firstName = request.form.get('firstName')
+        first_name = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
+        user = User.query.filter_by(email=email).first()
+
+        if user:
+            flash('Email already exists.', category='error')
+
         if len(email) < 4:
             flash('Email must be greater than 3 characters.', category='error')
-        elif len(firstName) < 2:
+        elif len(first_name) < 2:
             flash('First name must be greater than 1 character.', category='error')
         elif password1 != password2:
             flash('Passwords don\'t match.', category='error')
@@ -20,13 +29,30 @@ def signup():
             #make sure password is strong
             flash('Password must be at least 7 characters.', category='error')
         else:
+            new_user = User(email=email, first_name=first_name, password=sha256_crypt.hash(password1))
+            db.session.add(new_user)
+            db.session.commit()
             flash('Account created!', category='success')
+            return redirect(url_for('views.home'))
 
 
     return render_template("signup.html")
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if sha256_crypt.verify(password, user.password):
+                flash('Logged in successfully!', category='success')
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password or email', category='error')
+        else:
+            flash('Email does not exist.', category='error')
     return render_template("login.html", text=True)
 
 @auth.route('/logout')
