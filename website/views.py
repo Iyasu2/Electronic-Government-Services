892cb6f7328e4a26311169033c80e90f8a1fbad9
iyasu2 <iyasuforreal@gmail.com>
2023-11-22 21:36:11 +0300

    s

--- website/views.py
+++ website/views.py
@@ -7,7 +7,6 @@ import json
 views = Blueprint('views', __name__)
 
 @views.route('/', methods=['GET', 'POST'])
-@login_required
 def home():
     if request.method == 'POST':
         note = request.form.get('note')
