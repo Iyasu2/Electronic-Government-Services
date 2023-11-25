6e70f88c43b8c0c1045ad88060bce369e20f65a6
Ab-Ruh <abell.demere@gmail.com>
2023-11-25 20:55:52 +0300

    front

--- website/models.py
+++ website/models.py
@@ -1,6 +1,7 @@
 from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
+from enum import Enum
 
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -8,9 +9,53 @@ class Note(db.Model):
     date = db.Column(db.DateTime(timezone=True), default=func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
+class PendingStatus(Enum):
+    NOT_APPLIED = 'Not Applied'
+    APPLIED_PENDING = 'Applied and Pending'
+    APPLIED_ACCEPTED = 'Applied and Accepted'
+
+class Common():
+    id = db.Column(db.Integer, primary_key=True)
+    firstName = db.Column(db.String(50))
+    fatherName = db.Column(db.String(50))
+    gfatherName = db.Column(db.String(50))
+    birthDay = db.Column(db.Date)
+    gender = db.Column(db.String(10))
+    region = db.Column(db.String(50))
+    photo = db.Column(db.String(100))
+    pending = db.Column(db.Enum(PendingStatus), default=PendingStatus.NOT_APPLIED)
+
+class Common2():
+    id = db.Column(db.Integer, primary_key=True)
+    subCity = db.Column(db.String(50))
+    woreda = db.Column(db.String(50))
+    houseNumber = db.Column(db.String(50))
+    phoneNumber = db.Column(db.String(50))
+    bloodType = db.Column(db.String(10))
+
+class Driver_license_renewal(Common, Common2, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    expiryDate = db.Column(db.Date)
+    grade = db.Column(db.String(20))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+class National_id(Common, Common2, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    ecName = db.Column(db.String(50))
+    ecphoneNumber = db.Column(db.String(50))
+    expiryDate = db.Column(db.Date)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+class Birth_certificate(Common, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    fatherfullName = db.Column(db.String(50))
+    motherfullName = db.Column(db.String(50))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key = True)
-    email = db.Column(db.String(150), unique=True)
-    password = db.Column(db.String(150))
-    first_name = db.Column(db.String(150))
+    email = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(50))
+    first_name = db.Column(db.String(50))
     notes = db.relationship('Note')
+
--- website/views.py
+++ website/views.py
@@ -1,10 +1,22 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required, current_user, login_user, logout_user
-from .models import Note
+from .models import PendingStatus, Note, Birth_certificate, National_id, Driver_license_renewal
 from . import db
 import json
+import os
+from datetime import datetime
+from werkzeug.utils import secure_filename
+from flask_wtf import FlaskForm
+from wtforms import FileField, SubmitField
+from wtforms.validators import InputRequired
 
 views = Blueprint('views', __name__)
+current_file_dir = os.path.dirname(os.path.abspath(__file__))
+UPLOAD_FOLDER = os.path.join(current_file_dir, 'static/uploads')
+
+class UploadFileForm(FlaskForm):
+    file = FileField("FILE", validators=[InputRequired()])
+    submit = SubmitField("Upload File")
 
 @views.route('/home', methods=['GET', 'POST'])
 def home():
@@ -34,9 +46,120 @@ def delete_note():
             db.session.commit()
             return jsonify({})
 
-@views.route('/form', methods=['GET'])
+@views.route('/form/birth_certificate', methods=['GET', 'POST'])
 @login_required
-def forms():
-    variable = request.args.get('variable')
-    return render_template("form.html", user=current_user, variable=variable)
+def birth_certificate():
+    form = UploadFileForm()
+    photo = None
+    if request.method == 'POST':
+        firstName = request.form.get('firstName')
+        fatherName = request.form.get('fatherName')
+        gfatherName = request.form.get('gfatherName')
+        birthDay_str = request.form.get('birthDay')
+        birthDay = datetime.strptime(birthDay_str, '%Y-%m-%d').date()
+        gender = request.form.get('gender')
+        region = request.form.get('region')
+        pending = PendingStatus.APPLIED_PENDING
+        fatherfullName = request.form.get('fatherfullName')
+        motherfullName = request.form.get('motherfullName')
+        if form.validate_on_submit():
+            file = form.file.data
+            filename = secure_filename(f"user_{current_user.id}_{datetime.now().strftime('%Y%m%d%H%M%S')}.{file.filename.rsplit('.', 1)[1].lower()}")
+            file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),UPLOAD_FOLDER,filename))
+            photo = UPLOAD_FOLDER + '/' + filename
 
+        new_birth_certificate = Birth_certificate(firstName=firstName, fatherName=fatherName, gfatherName=gfatherName, birthDay=birthDay, gender=gender, region=region, photo=photo, pending=pending, fatherfullName=fatherfullName, motherfullName=motherfullName, user_id=current_user.id)
+        db.session.add(new_birth_certificate)
+        db.session.commit()
+        flash('Application completed!', category='success')
+        return redirect(url_for('views.home'))
+    return render_template("birth_certificate.html", user=current_user, form=form)
+    
+@views.route('/form/driver_license_renewal', methods=['GET', 'POST'])
+@login_required
+def driver_license_renewal():
+    form = UploadFileForm()
+    photo = None
+    variable = request.args.get('variable')
+    if request.method == 'POST':
+        firstName = request.form.get('firstName')
+        fatherName = request.form.get('fatherName')
+        gfatherName = request.form.get('gfatherName')
+        birthDay_str = request.form.get('birthDay')
+        birthDay = datetime.strptime(birthDay_str, '%Y-%m-%d').date()
+        gender = request.form.get('gender')
+        region = request.form.get('region')
+        pending = PendingStatus.APPLIED_PENDING
+        subCity = request.form.get('subCity')
+        woreda = request.form.get('woreda')
+        houseNumber = request.form.get('houseNumber')
+        phoneNumber = request.form.get('phoneNumber')
+        bloodType = request.form.get('bloodType')
+        expiryDate_str = request.form.get('expiryDate')
+        expiryDate = datetime.strptime(expiryDate_str, '%Y-%m-%d').date()
+        grade = request.form.get('grade')
+        if form.validate_on_submit():
+            file = form.file.data
+            filename = secure_filename(f"user_{current_user.id}_{datetime.now().strftime('%Y%m%d%H%M%S')}.{file.filename.rsplit('.', 1)[1].lower()}")
+            file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),UPLOAD_FOLDER,filename))
+            photo = UPLOAD_FOLDER + '/' + filename
+
+        new_driver_license_renewal = Driver_license_renewal(firstName=firstName, fatherName=fatherName, gfatherName=gfatherName, birthDay=birthDay, gender=gender, region=region, photo=photo, pending=pending, subCity=subCity, woreda=woreda, houseNumber=houseNumber, phoneNumber=phoneNumber, bloodType=bloodType, expiryDate=expiryDate, grade=grade, user_id=current_user.id)
+        db.session.add(new_driver_license_renewal)
+        db.session.commit()
+        flash('Application completed!', category='success')
+        return redirect(url_for('views.home'))
+    return render_template("driver_license_renewal.html", user=current_user, form=form)
+    
+@views.route('/form/national_id', methods=['GET', 'POST'])
+@login_required
+def national_id():
+    form = UploadFileForm()
+    photo = None
+    variable = request.args.get('variable')
+    if request.method == 'POST':
+        firstName = request.form.get('firstName')
+        fatherName = request.form.get('fatherName')
+        gfatherName = request.form.get('gfatherName')
+        birthDay_str = request.form.get('birthDay')
+        birthDay = datetime.strptime(birthDay_str, '%Y-%m-%d').date()
+        gender = request.form.get('gender')
+        region = request.form.get('region')
+        pending = PendingStatus.APPLIED_PENDING
+        subCity = request.form.get('subCity')
+        woreda = request.form.get('woreda')
+        houseNumber = request.form.get('houseNumber')
+        phoneNumber = request.form.get('phoneNumber')
+        bloodType = request.form.get('bloodType')
+        expiryDate_str = request.form.get('expiryDate')
+        expiryDate = datetime.strptime(expiryDate_str, '%Y-%m-%d').date()
+        ecName = request.form.get('ecName')
+        ecphoneNumber = request.form.get('ecphoneNumber')
+        if form.validate_on_submit():
+            file = form.file.data
+            filename = secure_filename(f"user_{current_user.id}_{datetime.now().strftime('%Y%m%d%H%M%S')}.{file.filename.rsplit('.', 1)[1].lower()}")
+            file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),UPLOAD_FOLDER,filename))
+            photo = UPLOAD_FOLDER + '/' + filename
+
+        new_national_id = National_id(firstName=firstName, fatherName=fatherName, gfatherName=gfatherName, birthDay=birthDay, gender=gender, region=region, photo=photo, pending=pending, subCity=subCity, woreda=woreda, houseNumber=houseNumber, phoneNumber=phoneNumber, bloodType=bloodType, expiryDate=expiryDate, ecName=ecName, ecphoneNumber=ecphoneNumber, user_id=current_user.id)
+        db.session.add(new_national_id)
+        db.session.commit()
+        flash('Application completed!', category='success')
+        return redirect(url_for('views.home'))
+    return render_template("national_id.html", user=current_user, form=form)
+
+
+@views.route('/applications', methods=['GET'])
+@login_required
+def applications():
+    applied_pending_models = []
+
+    table_models = [Driver_license_renewal, National_id, Birth_certificate]
+
+    for table_model in table_models:
+        table_name = table_model.__tablename__
+        table_pending_status = table_model.query.filter_by(pending=PendingStatus.APPLIED_PENDING).first()
+        if table_pending_status:
+            applied_pending_models.append(table_name)
+
+    return render_template('applications.html', tables=applied_pending_models, user=current_user)
