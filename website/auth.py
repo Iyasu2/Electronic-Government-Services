9f6d14a66d8162befac0bc7b35679d2a357e6209
Iyasu2 <126699641+Iyasu2@users.noreply.github.com>
2023-12-13 21:00:24 +0300

    Update auth.py

--- website/auth.py
+++ website/auth.py
@@ -1,3 +1,7 @@
+'''
+This module defines the authentication routes and functions for the Flask application.
+'''
+
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User, Admin_User
 from . import db, mail
@@ -5,12 +9,14 @@ from . import create_app as app
 from passlib.hash import sha256_crypt
 from flask_login import login_user, login_required, logout_user, current_user
 from flask_mail import Message
- 
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/signup', methods=['GET', 'POST'])
 def signup():
+    '''
+    This route handles the user signup process.
+    '''
     if request.method == 'POST':
         email = request.form.get('email')
         first_name = request.form.get('firstName')
@@ -33,7 +39,7 @@ def signup():
             flash('Passwords don\'t match.', category='error')
             return redirect(url_for('auth.signup'))
         elif len(password1) < 7:
-            #make sure password is strong
+            # Make sure password is strong
             flash('Password must be at least 7 characters.', category='error')
             return redirect(url_for('auth.signup'))
         else:
@@ -44,11 +50,13 @@ def signup():
             flash('Account created!', category='success')
             return redirect(url_for('views.home'))
 
-
     return render_template("signup.html", user=current_user)
 
 @auth.route('/admin/signup', methods=['GET', 'POST'])
 def signup_admin():
+    '''
+    This route handles the admin user signup process.
+    '''
     admin_user = Admin_User.query.first()
 
     if admin_user:
@@ -71,7 +79,7 @@ def signup_admin():
             flash('Passwords don\'t match.', category='error')
             return redirect(url_for('auth.signup_admin'))
         elif len(password1) < 7:
-            #make sure password is strong
+            # Make sure password is strong
             flash('Password must be at least 7 characters.', category='error')
             return redirect(url_for('auth.signup_admin'))
         else:
@@ -82,11 +90,13 @@ def signup_admin():
             flash('Admin Account created!', category='success')
             return redirect(url_for('views.home_admin'))
 
-
     return render_template("signup_admin.html", user=current_user)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+    '''
+    This route handles the user login process.
+    '''
     if current_user.is_authenticated:
         flash('You are already signed in!', category='error')
         return redirect(url_for('views.home'))
@@ -107,35 +117,52 @@ def login():
     return render_template("login.html", user=current_user)
 
 def send_email(user):
+    '''
+    This function sends a password reset email to the user.
+    '''
     token = user.get_token()
     msg = Message('Password Reset Request', recipients=[user.email], sender='noreply@egov.com')
-    msg.body = f''' To reset your password, Please follow the link below.
+    msg.body = f''' Toreset your password, visit the following link:
+{url_for('auth.reset_password', token=token, _external=True)}
 
-    {url_for('auth.reset_token', token=token, _external=True)}
-If you didn't send a password reset request, Please ignore this message.
-
+If you did not make this request, simply ignore this email and no changes will be made.
 '''
     mail.send(msg)
 
+@auth.route('/forgot_password', methods=['GET', 'POST'])
+def forgot_password():
+    '''
+    This route handles the forgot password process.
+    '''
+    if current_user.is_authenticated:
+        flash('You are already signed in!', category='error')
+        return redirect(url_for('views.home'))
 
-@auth.route('/reset_password', methods=['GET', 'POST'])
-def reset_password():
     if request.method == 'POST':
         email = request.form.get('email')
-
         user = User.query.filter_by(email=email).first()
         if user:
-            send_email(user) 
-            flash('Reset request sent. Check your email.', category='success')
+            send_email(user)
+            flash('An email has been sent with instructions to reset your password.', category='success')
+            return redirect(url_for('auth.login'))
+        else:
+            flash('Email does not exist.', category='error')
 
-    return render_template("reset_request.html", user=current_user)
+    return render_template("forgot_password.html", user=current_user)
 
 @auth.route('/reset_password/<token>', methods=['GET', 'POST'])
-def reset_token(token):
+def reset_password(token):
+    '''
+    This route handles the password reset process.
+    '''
+    if current_user.is_authenticated:
+        flash('You are already signed in!', category='error')
+        return redirect(url_for('views.home'))
+
     user = User.verify_token(token)
-    if user is None:
-        flash('That is an invalid or expired token!', category='error')
-        return redirect(url_for('reset_password'))
+    if not user:
+        flash('That is an invalid or expired token.', category='error')
+        return redirect(url_for('auth.forgot_password'))
 
     if request.method == 'POST':
         password1 = request.form.get('password1')
@@ -143,40 +170,24 @@ def reset_token(token):
 
         if password1 != password2:
             flash('Passwords don\'t match.', category='error')
-            return redirect(url_for('auth.reset_token'))
+            return redirect(url_for('auth.reset_password', token=token))
         elif len(password1) < 7:
-            #make sure password is strong
+            # Make sure password is strong
             flash('Password must be at least 7 characters.', category='error')
-            return redirect(url_for('auth.reset_token'))
+            return redirect(url_for('auth.reset_password', token=token))
         else:
             user.password = sha256_crypt.hash(password1)
             db.session.commit()
-            flash('Password changed!', category='success')
+            flash('Your password has been reset! You are now able to log in.', category='success')
             return redirect(url_for('auth.login'))
-        
-    return render_template("change_password.html", user=current_user)
 
- 
-@auth.route('/admin/login', methods=['GET', 'POST'])
-def login_admin():
-    if request.method == 'POST':
-        email = request.form.get('email')
-        password = request.form.get('password')
-
-        Admin_user = Admin_User.query.filter_by(email=email).first()
-        if Admin_user:
-            if sha256_crypt.verify(password, Admin_user.password):
-                flash('Logged in successfully!', category='success')
-                login_user(Admin_user, remember=True)
-                return redirect(url_for('views.home_admin'))
-            else:
-                flash('Incorrect password or email', category='error')
-        else:
-            flash('Email does not exist.', category='error')
-    return render_template("login_admin.html", user=current_user)
+    return render_template("reset_password.html", user=current_user)
 
 @auth.route('/logout')
 @login_required
 def logout():
+    '''
+    This route handles the user logout process.
+    '''
     logout_user()
-    return redirect(url_for('auth.login') )
+    return redirect(url_for('views.home'))
